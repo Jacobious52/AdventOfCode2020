37b888b60dc67a2e507d089917b376ed816b4b7f
Jacob Gonzalez <jgonzalez@atlassian.com>
2020-12-05 21:37:21 +1100

    part1 refactored

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,14 +1,13 @@
-use anyhow::anyhow;
 use reformation::Reformation;
+use std::collections::HashMap as Map;
+use std::mem::{discriminant, Discriminant};
 use std::str::FromStr;
 
 use advent_of_code_2020::advent::util;
 
 fn main() {
     color_backtrace::install();
-    let input: Vec<_> = include_str!("../../inputs/4.1.txt")
-        .split("\n\n")
-        .collect();
+    let input: Vec<_> = include_str!("../../inputs/4.1.txt").split("\n\n").collect();
     let passports: Vec<Passport> = input
         .into_iter()
         .map(|s| s.parse().expect("failed parsing passport"))
@@ -17,23 +16,44 @@ fn main() {
     println!("part01: {}", count_invalid(&passports));
 }
 
-#[derive(Debug, Reformation)]
-#[reformation(r"{key}:{value}")]
-struct Field {
-    key: String,
-    value: String,
+#[derive(Debug, Eq, PartialEq, Reformation)]
+enum Field {
+    #[reformation(r"byr:{}")]
+    Byr(i64),
+    #[reformation(r"iyr:{}")]
+    Iyr(i64),
+    #[reformation(r"eyr:{}")]
+    Eyr(i64),
+    #[reformation(r"hgt:{}")]
+    Hgt(String),
+    #[reformation(r"hcl:{}")]
+    Hcl(String),
+    #[reformation(r"ecl:{}")]
+    Ecl(String),
+    #[reformation(r"pid:{}")]
+    Pid(String),
+    #[reformation(r"cid:{}")]
+    Cid(String),
 }
 
-#[derive(Debug, Default)]
+impl Field {
+    fn is_valid(&self) -> bool {
+        match *self {
+            Field::Byr(_) => true,
+            Field::Iyr(_) => true,
+            Field::Eyr(_) => true,
+            Field::Hgt(_) => true,
+            Field::Hcl(_) => true,
+            Field::Ecl(_) => true,
+            Field::Pid(_) => true,
+            Field::Cid(_) => true,
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Passport {
-    byr: Option<Field>,
-    iyr: Option<Field>,
-    eyr: Option<Field>,
-    hgt: Option<Field>,
-    hcl: Option<Field>,
-    ecl: Option<Field>,
-    pid: Option<Field>,
-    cid: Option<Field>,
+    fields: Vec<Field>,
 }
 
 impl FromStr for Passport {
@@ -42,40 +62,29 @@ impl FromStr for Passport {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let fields: Vec<Field> = util::words(s)
             .iter()
-            .map(|w| Field::parse(w).expect("failed parse field"))
+            .map(|w| Field::parse(w).expect(&format!("failed parse field '{}'", &w)))
             .collect();
 
-        let mut passport = Passport::default();
-        for field in fields {
-            match field.key.as_ref() {
-                "byr" => passport.byr = Some(field),
-                "iyr" => passport.iyr = Some(field),
-                "eyr" => passport.eyr = Some(field),
-                "hgt" => passport.hgt = Some(field),
-                "hcl" => passport.hcl = Some(field),
-                "ecl" => passport.ecl = Some(field),
-                "pid" => passport.pid = Some(field),
-                "cid" => passport.cid = Some(field),
-                _ => return Err(anyhow!("unknown field {:?}", field)),
-            }
-        }
-
-        Ok(passport)
+        Ok(Passport { fields })
     }
 }
 
 impl Passport {
     fn is_valid(&self) -> bool {
-        self.byr
-            .as_ref()
-            .and(self.iyr.as_ref())
-            .and(self.eyr.as_ref())
-            .and(self.hgt.as_ref())
-            .and(self.hcl.as_ref())
-            .and(self.hgt.as_ref())
-            .and(self.ecl.as_ref())
-            .and(self.pid.as_ref())
-            .is_some()
+        let mut fields_found = Map::new();
+        fields_found.insert(discriminant(&Field::Byr(i64::default())), 0);
+        fields_found.insert(discriminant(&Field::Iyr(i64::default())), 0);
+        fields_found.insert(discriminant(&Field::Eyr(i64::default())), 0);
+        fields_found.insert(discriminant(&Field::Hgt(String::default())), 0);
+        fields_found.insert(discriminant(&Field::Hcl(String::default())), 0);
+        fields_found.insert(discriminant(&Field::Ecl(String::default())), 0);
+        fields_found.insert(discriminant(&Field::Pid(String::default())), 0);
+
+        for field in &self.fields {
+            *fields_found.entry(discriminant(field)).or_insert(0) += 1;
+        }
+
+        fields_found.values().filter(|c| **c != 1).count() == 0
     }
 }
 
@@ -101,5 +110,13 @@ mod test {
     }
 
     #[test]
-    fn test02() {}
+    fn test02() {
+        let input: Vec<_> = include_str!("../../inputs/4.1.txt").split("\n\n").collect();
+        let passports: Vec<Passport> = input
+            .into_iter()
+            .map(|s| s.parse().expect("failed parsing passport"))
+            .collect();
+
+        assert_eq!(219, count_invalid(&passports));
+    }
 }
