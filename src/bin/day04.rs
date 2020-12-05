05a7181bde762c7e192328273d8d1984c6ff9599
Jacob Gonzalez <jgonzalez@atlassian.com>
2020-12-05 22:35:06 +1100

    part2 done

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,6 +1,6 @@
 use reformation::Reformation;
 use std::collections::HashMap as Map;
-use std::mem::{discriminant, Discriminant};
+use std::mem::discriminant;
 use std::str::FromStr;
 
 use advent_of_code_2020::advent::util;
@@ -13,7 +13,8 @@ fn main() {
         .map(|s| s.parse().expect("failed parsing passport"))
         .collect();
 
-    println!("part01: {}", count_invalid(&passports));
+    println!("part01: {}", count_valid_part01(&passports));
+    println!("part02: {}", count_valid_part02(&passports));
 }
 
 #[derive(Debug, Eq, PartialEq, Reformation)]
@@ -38,14 +39,37 @@ enum Field {
 
 impl Field {
     fn is_valid(&self) -> bool {
-        match *self {
-            Field::Byr(_) => true,
-            Field::Iyr(_) => true,
-            Field::Eyr(_) => true,
-            Field::Hgt(_) => true,
-            Field::Hcl(_) => true,
-            Field::Ecl(_) => true,
-            Field::Pid(_) => true,
+        match self {
+            Field::Byr(v) => *v >= 1920 && *v <= 2002,
+            Field::Iyr(v) => *v >= 2010 && *v <= 2020,
+            Field::Eyr(v) => *v >= 2020 && *v <= 2030,
+            Field::Hgt(v) => {
+                if v.ends_with("cm") {
+                    let i = v.trim_end_matches("cm").parse::<i64>().expect("bad hgt in");
+                    return i >= 150 && i <= 193;
+                }
+                if v.ends_with("in") {
+                    let i = v.trim_end_matches("in").parse::<i64>().expect("bad hgt in");
+                    return i >= 59 && i <= 76;
+                }
+
+                false
+            }
+            Field::Hcl(v) => {
+                if !v.starts_with("#") {
+                    return false;
+                }
+                let v = &v[1..];
+                v.len() == 6
+                    && v.trim_matches(|c: char| c.is_numeric() || ('a'..='f').contains(&c))
+                        .len()
+                        == 0
+            }
+            Field::Ecl(v) => match v.as_ref() {
+                "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" => true,
+                _ => false,
+            },
+            Field::Pid(v) => v.len() == 9 && v.trim_matches(char::is_numeric).len() == 0,
             Field::Cid(_) => true,
         }
     }
@@ -84,12 +108,24 @@ impl Passport {
             *fields_found.entry(discriminant(field)).or_insert(0) += 1;
         }
 
-        fields_found.values().filter(|c| **c != 1).count() == 0
+        fields_found.values().all(|c| *c == 1)
+    }
+
+    fn is_fields_valid(&self) -> bool {
+        self.fields.iter().all(|f| f.is_valid())
     }
 }
 
-fn count_invalid(passports: &[Passport]) -> usize {
+fn count_valid_part01(passports: &[Passport]) -> usize {
     passports.iter().filter(|p| p.is_valid()).count()
+}
+
+fn count_valid_part02(passports: &[Passport]) -> usize {
+    passports
+        .iter()
+        .filter(|p| p.is_valid())
+        .filter(|p| p.is_fields_valid())
+        .count()
 }
 
 #[cfg(test)]
@@ -106,7 +142,7 @@ mod test {
             .map(|s| s.parse().expect("failed parsing passport"))
             .collect();
 
-        assert_eq!(2, count_invalid(&passports));
+        assert_eq!(2, count_valid_part01(&passports));
     }
 
     #[test]
@@ -117,6 +153,53 @@ mod test {
             .map(|s| s.parse().expect("failed parsing passport"))
             .collect();
 
-        assert_eq!(219, count_invalid(&passports));
+        assert_eq!(219, count_valid_part01(&passports));
+    }
+
+    #[test]
+    fn test_valid() {
+        let input: Vec<_> = include_str!("../../inputs/4.valid.txt")
+            .split("\n\n")
+            .collect();
+        let passports: Vec<Passport> = input
+            .into_iter()
+            .map(|s| s.parse().expect("failed parsing passport"))
+            .collect();
+
+        assert_eq!(passports.len(), count_valid_part02(&passports));
+    }
+
+    #[test]
+    fn test_invalid() {
+        let input: Vec<_> = include_str!("../../inputs/4.invalid.txt")
+            .split("\n\n")
+            .collect();
+        let passports: Vec<Passport> = input
+            .into_iter()
+            .map(|s| s.parse().expect("failed parsing passport"))
+            .collect();
+
+        assert_eq!(0, count_valid_part02(&passports));
+    }
+
+    #[test]
+    fn test_fields_valid() {
+        assert_eq!(true, Field::Byr(2002).is_valid());
+        assert_eq!(false, Field::Byr(2003).is_valid());
+
+        assert_eq!(true, Field::Hgt("60in".into()).is_valid());
+        assert_eq!(true, Field::Hgt("190cm".into()).is_valid());
+        assert_eq!(false, Field::Hgt("190in".into()).is_valid());
+        assert_eq!(false, Field::Hgt("190".into()).is_valid());
+
+        assert_eq!(true, Field::Hcl("#123abc".into()).is_valid());
+        assert_eq!(false, Field::Hcl("#123abz".into()).is_valid());
+        assert_eq!(false, Field::Hcl("123abc".into()).is_valid());
+
+        assert_eq!(true, Field::Ecl("brn".into()).is_valid());
+        assert_eq!(false, Field::Ecl("wat".into()).is_valid());
+
+        assert_eq!(true, Field::Pid("000000001".into()).is_valid());
+        assert_eq!(false, Field::Pid("0123456789".into()).is_valid());
     }
 }
